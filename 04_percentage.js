260f491ebf1d45637c98f8332dc081edb25e8e46
Indykatz <cats83@me.com>
2022-05-19 09:48:44 +0100

    working v1

---
+++ 04_percentage.js
@@ -0,0 +1,10 @@
+// percentage function
+
+const percent = (a, b) => {
+  return (a / 100) * b;
+};
+
+console.log(percent(100, 100));
+console.log(percent(50, 100));
+console.log(percent(72, 95));
+console.log(percent(24, 86));
